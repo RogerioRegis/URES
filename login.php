b1fd411decfa769264d6e5fbf38d047339c04119
Rogerio Regis <rog_reg@hotmail.com>
2014-06-18 09:43:59 -0400

    [ADD] Inicio do projeeto
    Signed-off-by: Rogerio Regis <rog_reg@hotmail.com>

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php include_once './header.php'; ?>
+<?php include_once './menu.php'; ?>
+
+   <title>Entrar - Ures Roraima</title>
+   
+    <div class="row marketing">
+      <div class="col-lg-6">
+       <form class="form-signin" role="form">
+        <h3 class="form-signin-heading">Entrar</h3>
+        <input type="email" class="form-control" placeholder="Email" required autofocus><br/>
+        <input type="password" class="form-control" placeholder="Senha" required>
+        
+        <label class="checkbox"><input type="checkbox" value="remember-me"> Lembrar senha</label>
+        
+        <button class="btn btn-success" type="submit">Entrar</button >
+
+        <ul class="list-unstyled">
+            <br/>
+            <li><a href="cadastro.html">Criar uma conta</a></li>
+            <li><a href="cadastro.html">Esqueceu seu usuário?</a></li>
+            <li><a href="cadastro.html">Esqueceu sua senha?</a></li>
+        </ul>
+        
+       </form>                
+      </div>
+    </div>
+   
+   
+   
+   
+   
+   
+   
+
+<?php include_once './footer.php'; ?>
